cdfbb608c68eec2ce9d313434de1ce53fdbd9b8c
changshu.li <lcs.005@163.com>
2020-01-17 00:26:23 +0800

    operation

---
+++ src/main/java/com/github/microwww/ttp/opt/Operation.java
@@ -0,0 +1,23 @@
+package com.github.microwww.ttp.opt;
+
+public class Operation {
+
+    private String operation;
+    private Object[] objects;
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public Object[] getObjects() {
+        return objects;
+    }
+
+    public void setObjects(Object[] objects) {
+        this.objects = objects;
+    }
+}
---
+++ src/main/java/com/github/microwww/ttp/opt/RepeatOperation.java
@@ -0,0 +1,4 @@
+package com.github.microwww.ttp.opt;
+
+public class RepeatOperation extends Operation {
+}
