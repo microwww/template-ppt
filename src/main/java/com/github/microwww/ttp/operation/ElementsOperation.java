1100defc89668bfbbcef1583b2e0c481c0b5ab61
changshu.li <lcs.005@163.com>
2020-03-01 02:06:46 +0800

    Delete DeleteShape / ElementsOperation

--- src/main/java/com/github/microwww/ttp/operation/ElementsOperation.java
+++
@@ -1,9 +0,0 @@
-package com.github.microwww.ttp.operation;
-
-public interface ElementsOperation<T, C> {
-    void copy(T src, C shapes);
-
-    void delete(T src, C shapes);
-
-    void replace(T src, C shapes);
-}
